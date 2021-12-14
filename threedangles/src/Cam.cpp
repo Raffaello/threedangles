346e563369a64d10ae5cbf5b60a09975cd0bd65f
Raffaello Bertini <raffaellobertini@gmail.com>
2021-12-14 15:27:51 +0000

    [WIP] FPS Cam

--- threedangles/src/Cam.cpp
+++ threedangles/src/Cam.cpp
@@ -1,1 +1,44 @@
 #include <Cam.hpp>
+
+Cam::Cam(const Vec4& verticalOffset, const Vec4& targetOffset, const Vec4& cam_position) : verticalOffset(verticalOffset), targetOffset(targetOffset), cam(cam_position)
+{
+    eye = verticalOffset + cam_position;
+    target = eye + targetOffset;
+    up = Vec4(0.0f, 1.0f, 0.0f);
+    CreateLookAt(eye, target, up);
+}
+
+void Cam::CreateLookAt(const Vec4& eye, const Vec4& target, const Vec4& Up) noexcept
+{
+    Vec4 F = (target - eye).normalize();
+    Vec4 L = Up.crossProd(F).normalize();
+    Vec4 U = F.crossProd(L);
+    Vec4 T(-L.dotProd(eye), -U.dotProd(eye), -F.dotProd(eye));
+
+    //// Create and return look-at matrix from F, L, U, and T
+    matLookAt.m[0][0] = L.x; matLookAt.m[0][1] = U.x; matLookAt.m[0][2] = F.x; matLookAt.m[0][3] = 0.0f;
+    matLookAt.m[1][0] = L.y; matLookAt.m[1][1] = U.y; matLookAt.m[1][2] = F.y; matLookAt.m[1][3] = 0.0f;
+    matLookAt.m[2][0] = L.z; matLookAt.m[2][1] = U.z; matLookAt.m[2][2] = F.z; matLookAt.m[2][3] = 0.0f;
+    matLookAt.m[3][0] = T.x; matLookAt.m[3][1] = T.y; matLookAt.m[3][2] = T.z; matLookAt.m[3][3] = 1.0f;
+}
+
+void Cam::Update(const float delta_yaw, const float delta_pitch) noexcept
+{
+    yaw += delta_yaw;
+    pitch += delta_pitch;
+    // TODO: limit pitch to a min max pitch
+    /*if (pitch < minPitch) {
+        pitch = minPitch;
+    }
+    else if (pitch > maxPitch) {
+        pitch = maxPitch;
+    }*/
+
+    // TODO: use Quaternion
+    Mat4 matY = Mat4::createRotationY(yaw);
+    Vec4 actualOffset = matY * targetOffset;
+    Vec4 F = actualOffset.normalize();
+    Vec4 L = up.crossProd(F).normalize();
+    // TODO: to be finished transformation around pitch
+
+}
--- threedangles/src/Cam.hpp
+++ threedangles/src/Cam.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <Vec3d.hpp>
+#include <Vec4.hpp>
 #include <Mat4.hpp>
 
 /**
@@ -12,16 +12,35 @@ class Cam
     // Must "return" the View matrix
     // from a player position and "lookAt"
 public:
+    Cam(const Vec4& verticalOffset, const Vec4& targetOffset, const Vec4& cam_position);
 
-    Vec3d cam;
-    Vec3d lookat;
-    Vec3d up;
-    Vec3d target;
+    // Camera offset from player character position
+    // For a y-up world, the vector would be (0, value, 0)
+    Vec4 verticalOffset;
 
-    float yaw;
-    float pitch;
+
+    Vec4 cam;
+    //Vec4 lookat;
+    Vec4 up;
+    // Target position offset from camera
+    // For a z-forward world, vector would be (0, 0, value)
+    Vec4 targetOffset;
+    Vec4 eye;
+    Vec4 target;
+
+    float yaw = 0.0f;
+    float pitch = 0.0f;
 
     Mat4 matCamRot;
     Mat4 matCam;
     Mat4 matView;
+
+    void CreateLookAt(const Vec4& eye, const Vec4& target, const Vec4& Up) noexcept;
+    void Update(const float delta_yaw, const float delta_pitch) noexcept;
+
+private:
+    Mat4 matLookAt;
+
+    float minPitch = 0.0f;
+    float maxPitch = 0.0f;
 };
