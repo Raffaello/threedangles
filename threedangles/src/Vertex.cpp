e58c45f2e29a8fac3d00a41828eb172f1eadfb8d
Raffaello Bertini <raffaellobertini@gmail.com>
2021-12-20 02:26:02 +0000

    init

---
+++ threedangles/src/Vertex.cpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Vec4.hpp>
+#include <Color.hpp>
+
+
+class Vertex
+{
+public:
+    Vec4 vec;
+    colort_t col;
+
+
+}
---
+++ threedangles/src/Vertex.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Vec4.hpp>
+#include <Color.hpp>
+
+class Vertex
+{
+public:
+    Vec4 v;
+    Vec4 normal;
+    Color col;
+}
